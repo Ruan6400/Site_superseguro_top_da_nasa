38310bb654c790e18d60cf54a7cd69a4c1d0f856
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2025-04-13 18:13:00 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const {Client,Pool} = require('pg');
 const bcrypt = require('bcrypt');
 const multer = require('multer');
 const path = require('path');
+const nodemailer = require('nodemailer');
 require('dotenv').config()
 
 const app = express();
@@ -18,6 +19,21 @@ const dbconfig = {
 const pool = new Pool(dbconfig);
 const saltRounds = process.env.SALT_ROUNDS*1;
 const upload = multer()
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+const mailOptions = {
+    from: 'ruansantos80@gmail.com',
+    to: 'goblinfuleiro@gmail.com',
+    subject: 'Assunto do E-mail',
+    text: 'Olá! Este é um e-mail enviado pelo Nodemailer.',
+};
+
 
 
 
@@ -44,7 +60,7 @@ async function criaBanco(){
 async function criaTabela(){
     pool.query(`CREATE TABLE IF NOT EXISTS usuarios(
         id SERIAL,
-        nome VARCHAR(50) NOT NULL,
+        nome VARCHAR(50) NOT NULL UNIQUE,
         email VARCHAR(100) NOT NULL UNIQUE,
         senha VARCHAR(255) NOT NULL,
         CONSTRAINT pk_usuarios PRIMARY KEY (id)
@@ -57,24 +73,28 @@ async function gerarHash(senha) {
 }
 
 app.post('/login',async (req,res)=>{
-    const {nome,senha} = req.body;
-    const cripto = await gerarHash(senha);
-    console.log(cripto);
-    // const consulta = await pool.query(`SELECT * FROM usuarios WHERE nome = $1 AND senha = $2`, [nome,senha]);
-    // if(consulta.rowCount <=0){
-    //     res.send('Usuário ou senha inválidos!');
-    // }else{
-    //     res.sendFile(path.join(__dirname,'login.html'))
-    // }
+    const {user,senha} = req.body;
+
+    const consulta = await pool.query(`SELECT * FROM usuarios WHERE nome = $1 OR email = $1`, [user]);
+    if(consulta.rowCount <=0){
+        res.send('Usuário ou senha inválidos!');
+    }else{
+        const valid_passwd = await bcrypt.compare(senha, consulta.rows[0].senha);
+        if(valid_passwd){
+            res.sendFile(path.join(__dirname,"Loginok.html"));
+        }else{
+            res.send('Usuário ou senha inválidos!');
+        }
+    }
+       
+    
 })
 
 app.post('/cadastrar',upload.none(),async (req,res)=>{
-    const {nome,email,senha} = req
-    console.log(req.body);
+    const {nome,email,senha} = req.body
     const cripto = await gerarHash(senha);
-    console.log(cripto);
-    /*await pool.query(`INSERT INTO usuarios (nome,email,senha) VALUES ($1,$2,$3);`, [nome,email,cripto]);
-    res.send("cadastro realizado com sucesso!")*/
+    await pool.query(`INSERT INTO usuarios (nome,email,senha) VALUES ($1,$2,$3);`, [nome,email,cripto]);
+    res.sendFile(path.join(__dirname,"Loginok.html"))
 })
 
 
@@ -83,5 +103,12 @@ app.listen(port, () => {
     console.log('Server is running on port '+port);
     criaBanco().then(() => {
         criaTabela()
+    });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Erro ao enviar e-mail:', error);
+        } else {
+            console.log('E-mail enviado com sucesso:', info.response);
+        }
     });
 })
